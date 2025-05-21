8838c2f31aaac8cda80b8c06227b9d79344e0b61
genuinelycool <genuinelycool@gmail.com>
2025-05-22 04:49:52 +0545

    09 - 006 Adding new component to insert data

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -4,13 +4,8 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-                    <div class="relative">
-                        <input
-                            type="text"
-                            class="form-control form-control-lg padding-right-lg"
-                            placeholder="+ Add new task. Press enter to save."
-                        />
-                    </div>
+                    <NewTask @added="handleAddedTask" />
+
                     <!-- List of uncompleted tasks -->
                     <Tasks :tasks="uncompletedTasks" />
 
@@ -34,8 +29,9 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
-import { allTasks } from "../http/task-api";
+import { allTasks, createTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
+import NewTask from "../components/tasks/NewTask.vue";
 
 const tasks = ref([]);
 
@@ -55,4 +51,9 @@ const completedTasksIsVisible = computed(
 );
 
 const showCompletedTasks = ref(false);
+
+const handleAddedTask = async (newTask) => {
+    const { data : createdTask } = await createTask(newTask);
+    tasks.value.unshift(createdTask.data);
+}
 </script>
