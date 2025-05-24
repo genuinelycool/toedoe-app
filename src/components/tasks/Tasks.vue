ae2ce59a1257f718cbcd05091a85b652caf0cfcd
genuinelycool <genuinelycool@gmail.com>
2025-05-24 20:59:21 +0545

    10 - 006 Refactoring our components

--- src/components/tasks/Tasks.vue
+++ src/components/tasks/Tasks.vue
@@ -1,26 +1,30 @@
 <template>
     <div class="card mt-2" v-if="tasks.length && show">
         <ul class="list-group list-group-flush">
-            <Task 
-                v-for="task in tasks" 
-                :task="task" 
-                :key="task.id" 
-                @updated="$emit('updated', $event)"
-                @completed="$emit('completed', $event)"
-                @remove="$emit('remove', $event)"
+            <Task
+                v-for="task in tasks"
+                :task="task"
+                :key="task.id"
+                @updated="handleUpdatedTask"
+                @completed="handleCompletedTask"
+                @removed="handleRemovedTask"
             />
         </ul>
     </div>
 </template>
 
 <script setup>
+import { useTaskStore } from "../../stores/task";
 import Task from "./Task.vue";
+
+const store = useTaskStore();
+const { handleUpdatedTask, handleCompletedTask, handleRemovedTask } = store;
 
 defineProps({
     tasks: Array,
     show: {
         type: Boolean,
-        default: true
-    }
-})
+        default: true,
+    },
+});
 </script>
