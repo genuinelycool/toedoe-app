3d55223d7803dd5ad61c43eb5763fc309dd850bf
genuinelycool <genuinelycool@gmail.com>
2025-05-24 13:01:17 +0545

    10 - 002 Defining Stores

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -44,6 +44,8 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useTaskStore } from "../stores/task";
 import { 
     allTasks, 
     createTask, 
@@ -54,11 +56,22 @@ import {
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
 
+const store = useTaskStore();
+const { task } = storeToRefs(store);
+
+// store.$patch({
+//     task: {
+//         name: "First task updated using $patch",
+//         is_completed: true
+//     }
+// })
+
 const tasks = ref([]);
 
 onMounted(async () => {
     const { data } = await allTasks();
     tasks.value = data.data;
+    console.log(task.value);
 })
 
 const uncompletedTasks = computed(() => tasks.value.filter((task) => !task.is_completed));
